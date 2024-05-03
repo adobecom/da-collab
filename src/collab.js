fe63845bacf3c7645dc7ea8cd1af3b01a6299f23
Karl Pauls <kpauls@adobe.com>
2024-05-03 11:26:02 +0200

    share state

--- src/collab.js
+++ src/collab.js
@@ -9,44 +9,19 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import { prosemirrorJSONToYXmlFragment, yDocToProsemirrorJSON } from 'y-prosemirror';
-import { Schema } from 'prosemirror-model';
+import {
+  prosemirrorToYXmlFragment, yDocToProsemirror,
+} from 'y-prosemirror';
+import {
+  DOMParser, DOMSerializer, Schema,
+} from 'prosemirror-model';
 import { addListNodes } from 'prosemirror-schema-list';
 import {
   tableNodes,
 } from 'prosemirror-tables';
 import { schema as baseSchema } from 'prosemirror-schema-basic';
-
-function toTag(dom, content, type) {
-  let attributes = dom.length > 1 ? ' ' : '';
-  const attrs = {};
-  if (dom.length > 1) {
-    Object.entries(dom[1]).forEach(([key, value]) => {
-      attributes += value ? `${key}="${value}" ` : `${key}`;
-      attrs[key] = value;
-    });
-  }
-  if (attributes === ' ') attributes = '';
-  if (dom[0] === 'img') {
-    return `<picture><source srcset="${attrs.src}"><source srcset="${attrs.src}" media="(min-width: 600px)"><${dom[0]}${attributes}${content ? `>${content}</${dom[0]}>` : '/>'}</picture>`;
-  }
-  if (dom[0] === 'p' && content === '---') {
-    return '<hr/>';
-  }
-  return `<${dom[0]}${attributes}${type && type.isBlock ? `>${content}</${dom[0]}>` : '/>'}`;
-}
-
-function node2html(node, schema) {
-  const schemaNode = schema.nodes[node.type];
-  if (schemaNode.isBlock && node.content) {
-    const mapped = schemaNode.spec.toDOM ? schemaNode.spec.toDOM(node) : ['div'];
-    return toTag(mapped, node.content.map((child) => node2html(child, schema)).join(''), schemaNode);
-  }
-  if (schemaNode.spec.toDOM) return toTag(schemaNode.spec.toDOM(node));
-  if (!node.type === 'text') return '';
-  if (!node.marks) return node.text;
-  return `${node.marks.map((mark) => `<${mark.type}>`).join('')}${node.text}${node.marks.map((mark) => `</${mark.type}>`).join('')}`;
-}
+import { fromHtml } from 'hast-util-from-html';
+import { matches } from 'hast-util-select';
 
 function getSchema() {
   const { marks, nodes: baseNodes } = baseSchema.spec;
@@ -57,122 +32,194 @@ function getSchema() {
   return new Schema({ nodes, marks: customMarks });
 }
 
-export async function aem2prose(html, schema, rewriter = new HTMLRewriter()) {
-  const json = { type: 'doc', content: [] };
-  let current = json;
-  let currentText;
-  let marks = [];
-  class NodeHandler {
-    constructor(rule, node, mark = false) {
-      this.rule = rule;
-      this.node = node;
-      this.mark = mark;
-    }
+export async function aem2doc(html, ydoc) {
+  const tree = fromHtml(html, { fragment: true });
+  const main = tree.children.find((child) => child.tagName === 'main');
+  (main?.children || []).forEach((parent) => {
+    if (parent.tagName === 'div' && parent.children) {
+      const children = [];
+      let modified = false;
+      parent.children.forEach((child) => {
+        if (child.tagName === 'div' && child.properties.className?.length > 0) {
+          modified = true;
+          children.push({
+            type: 'element', tagName: 'p', children: [], properties: {},
+          });
+          const classes = Array.from(child.properties.className);
+          const name = classes.shift();
+          const blockName = classes.length > 0 ? `${name} (${classes.join(', ')})` : name;
+          const rows = [...child.children];
+          const maxCols = rows.reduce((cols, row) => (
+            row.children?.length > cols ? row.children?.length : cols), 0);
 
-    element(element) {
-      if (this.mark) {
-        if (currentText) {
-          current.content.push({ type: 'text', text: currentText, marks });
-          currentText = undefined;
-        }
+          const table = {
+            type: 'element', tagName: 'table', children: [], properties: {},
+          };
+          children.push(table);
+          const headerRow = {
+            type: 'element', tagName: 'tr', children: [], properties: {},
+          };
 
-        const parent = marks;
-        currentText = undefined;
-        marks = Array.from(marks);
-        const currentMark = { type: this.node.name, attrs: {} };
-        Object.entries(this.node.attrs).forEach(([name, value]) => {
-          const attrs = {};
-          for (const [attrName, attrValue] of element.attributes) {
-            attrs[attrName] = attrValue;
-          }
-          if (attrs[name]) {
-            currentMark.attrs[name] = attrs[name];
-          } else if (value.hasDefault) {
-            currentMark.attrs[name] = value.default;
-          }
-        });
-        marks.push(currentMark);
-        element.onEndTag(() => {
-          if (currentText) {
-            current.content.push({ type: 'text', text: currentText, marks });
-            currentText = undefined;
-          }
-          marks = parent;
-        });
-        return;
-      }
-      const parent = current;
-      current = {
-        type: this.node.name, content: [], attrs: {}, marks: Array.from(marks),
-      };
-      Object.entries(this.node.attrs).forEach(([name, value]) => {
-        const attrs = {};
-        for (const [attrName, attrValue] of element.attributes) {
-          attrs[attrName] = attrValue;
-        }
-        if (attrs[name]) {
-          current.attrs[name] = attrs[name];
-        } else if (value.hasDefault) {
-          current.attrs[name] = value.default;
+          const td = {
+            type: 'element', tagName: 'td', children: [{ type: 'text', value: name }], properties: { colspan: maxCols },
+          };
+
+          headerRow.children.push(td);
+          table.children.push(headerRow);
+          rows.forEach((row) => {
+            const tr = { tagName: 'tr', children: [], properties: {} };
+            const cells = row.children ? [...row.children] : [row];
+            cells.forEach((cell, idx) => {
+              const tdi = {
+                type: 'element', tagName: 'td', children: [], properties: {},
+              };
+              if (cells.length < maxCols && idx === cells.length - 1) {
+                tdi.properties.colspan = maxCols - idx;
+              }
+              tdi.children.push(cells[idx]);
+              tr.children.push(tdi);
+            });
+            table.children.push(tr);
+          });
+          children.push({
+            type: 'element', tagName: 'p', children: [], properties: {},
+          });
+          console.log(blockName);
+        } else {
+          children.push(child);
         }
       });
-      if (currentText) {
-        parent.content.push({ type: 'text', text: currentText, marks });
-        currentText = undefined;
-      }
-      parent.content.push(current);
-      if (this.node.isBlock) {
-        element.onEndTag(() => {
-          if (currentText) {
-            current.content.push({ type: 'text', text: currentText, marks });
-            currentText = undefined;
-          }
-          marks = [];
-        });
-      } else {
-        current = parent;
-        marks = [];
+      if (modified) {
+        // eslint-disable-next-line no-param-reassign
+        parent.children = children;
       }
     }
+  });
+  // convert section breaks
+  // convert sections
+  const handler2 = {
+    get(target, prop) {
+      const source = target;
+      if (prop === 'firstChild') {
+        if (target.children.length === 0) return null;
+        for (let i = 0; i < target.children.length - 1; i += 1) {
+          source.children[i].nextSibling = new Proxy(target.children[i + 1], handler2);
+          if (i > 0) {
+            source.children[i].previousSibling = new Proxy(target.children[i - 1], handler2);
+          } else {
+            source.children[i].previousSibling = new Proxy(
+              target.children[target.children.length - 1],
+              handler2,
+            );
+          }
+        }
+        return new Proxy(target.children[0], handler2);
+      }
+      if (prop === 'nodeType') {
+        return target.type === 'text' ? 3 : 1;
+      }
+      if (prop === 'nodeValue') {
+        return target.value;
+      }
 
-    text(text) {
-      currentText = currentText ? `${currentText}${text.text}` : text.text;
+      if (prop === 'nextSibling') {
+        return target.nextSibling;
+      }
+
+      if (prop === 'previousSibling') {
+        return target.previousSibling;
+      }
+
+      if (prop === 'nodeName') {
+        return target.tagName?.toUpperCase();
+      }
+
+      if (prop === 'matches') {
+        return (selector) => matches(selector, target);
+      }
+
+      if (prop === 'getAttribute') {
+        return (name) => (target.properties ? target.properties[name] : undefined);
+      }
+
+      if (prop === 'style') {
+        return {};
+      }
+
+      return Reflect.get(target, prop);
+    },
+  };
+  const json = DOMParser.fromSchema(getSchema()).parse(new Proxy(tree, handler2));
+  prosemirrorToYXmlFragment(json, ydoc.getXmlFragment('prosemirror'));
+}
+
+function tohtml(node) {
+  let attributes = Object.entries(node.attributes).map(([key, value]) => ` ${key}="${value}"`).join('');
+  if (!node.children || node.children.length === 0) {
+    if (node.type === 'text') {
+      return node.text;
+    }
+    if (node.type === 'p') return '';
+    if (node.type === 'img' && !node.attributes.loading) {
+      attributes += ' loading="lazy"';
+    }
+    const result = `<${node.type}${attributes}${node.type !== 'br' ? '/' : ''}>`;
+    if (node.type === 'img') {
+      return `<picture><source srcset="${node.attributes.src}"><source srcset="${node.attributes.src}" media="(min-width: 600px)">${result}</picture>`;
+    }
+
+    return result;
+  }
+  let { children } = node;
+  if (node.type === 'li') {
+    if (children.length === 1) {
+      if (children[0].type === 'p') {
+        children = children[0].children;
+      }
     }
   }
-
-  Object.values(schema.nodes).filter((node) => node.spec.parseDOM).forEach((node) => {
-    node.spec.parseDOM.forEach((rule) => {
-      rewriter.on(rule.tag, new NodeHandler(rule, node));
-    });
-  });
-  Object.values(schema.marks).filter((mark) => mark.spec.parseDOM).forEach((mark) => {
-    mark.spec.parseDOM.filter((rule) => rule.tag).forEach((rule) => {
-      rewriter.on(rule.tag, new NodeHandler(rule, mark, true));
-    });
-  });
-  const resp = await rewriter.transform(new Response(html));
-  await resp.text();
-  return json;
+  return `<${node.type}${attributes}>${children.map((child) => tohtml(child)).join('')}</${node.type}>`;
 }
-
-export async function aem2doc(html, ydoc, rewriter = new HTMLRewriter()) {
+export function doc2aem(ydoc) {
   const schema = getSchema();
-  const json = await aem2prose(html, schema, rewriter);
-  prosemirrorJSONToYXmlFragment(schema, json, ydoc.getXmlFragment('prosemirror'));
-}
+  const json = yDocToProsemirror(schema, ydoc);
 
-export function prose2aem(json, schema) {
-  const html = `
+  const fragment = { type: 'div', children: [], attributes: {} };
+  const handler3 = {
+    get(target, prop) {
+      const source = target;
+      if (prop === 'createDocumentFragment') {
+        return () => new Proxy(fragment, handler3);
+      }
+      if (prop === 'appendChild') {
+        return (node) => target.children.push(node);
+      }
+      if (prop === 'createElement') {
+        return (type) => new Proxy({ type, children: [], attributes: [] }, handler3);
+      }
+      if (prop === 'createTextNode') {
+        return (content) => new Proxy({ type: 'text', text: content, attributes: {} }, handler3);
+      }
+      if (prop === 'setAttribute') {
+        return (name, value) => {
+          source.attributes[name] = value;
+        };
+      }
+      return Reflect.get(target, prop);
+    },
+  };
+
+  DOMSerializer.fromSchema(schema)
+    .serializeFragment(json.content, { document: new Proxy({}, handler3) });
+
+  // convert table to blocks
+  // convert sections
+  const text = tohtml(fragment);
+  return `
 <body>
   <header></header>
-  <main>${node2html(json, schema)}</main>
+  <main>${text}</main>
   <footer></footer>
 </body>
 `;
-  return html;
-}
-export function doc2aem(ydoc) {
-  const json = yDocToProsemirrorJSON(ydoc);
-  const schema = getSchema();
-  return prose2aem(json, schema);
 }
---
+++ test/collab.test.js
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2024 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import assert from 'assert';
+import * as Y from 'yjs';
+import { aem2doc, doc2aem } from '../src/collab.js';
+
+describe('collab test suite', () => {
+    it('Test empty roundtrip', async () => {
+        const html = `
+<body>
+  <header></header>
+  <main><div></div></main>
+  <footer></footer>
+</body>
+`;
+      const yDoc = new Y.Doc();
+      await aem2doc(html, yDoc);
+      const result = doc2aem(yDoc);
+      assert.equal(result, html);
+    });
+    it('Test simple roundtrip', async () => {
+        const html = `
+<body>
+  <header></header>
+  <main><div><p>Hi</p><p>Test</p><p>World</p><p>test</p></div></main>
+  <footer></footer>
+</body>
+`;
+      const yDoc = new Y.Doc();
+      await aem2doc(html, yDoc);
+      const result = doc2aem(yDoc);
+      assert.equal(result, html);
+    });
+    it('Test more complex roundtrip', async () => {
+        const html = `
+<body>
+  <header></header>
+  <main><div><p><picture><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_1dc0a2d290d791a050feb1e159746f52db392775a.jpeg?width=750&format=jpeg&optimize=medium"><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_1dc0a2d290d791a050feb1e159746f52db392775a.jpeg?width=750&format=jpeg&optimize=medium" media="(min-width: 600px)"><img src="https://main--aem-block-collection--adobe.hlx.live/media_1dc0a2d290d791a050feb1e159746f52db392775a.jpeg?width=750&format=jpeg&optimize=medium" alt="Decorative double Helix" loading="lazy"></picture></p><h1>Congrats, you are ready to go! </h1><p>Your forked repo is setup as a helix project and you are ready to start developing.<br>The content you are looking at is served from this <a href="https://drive.google.com/drive/folders/1Gwwrujv0Z4TxJM8askdqQkHSD969dGK7">gdrive</a><br><br>Adjust the <code>fstab.yaml</code> to point to a folder either in your sharepoint or your gdrive that you shared with helix. See the full tutorial here:<br><br><a href="https://bit.ly/3aImqUL">https://www.hlx.live/tutorial</a></p><h2>This is another headline here for more content</h2><div class="columns"><div><div><p>Columns block</p><ul><li>One</li><li>Two</li><li>Three</li></ul><p><a href="/">Live</a></p></div><div><picture><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_17e9dd0aae03d62b8ebe2159b154d6824ef55732d.png?width=750&format=png&optimize=medium"><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_17e9dd0aae03d62b8ebe2159b154d6824ef55732d.png?width=750&format=png&optimize=medium" media="(min-width: 600px)"><img src="https://main--aem-block-collection--adobe.hlx.live/media_17e9dd0aae03d62b8ebe2159b154d6824ef55732d.png?width=750&format=png&optimize=medium" alt="green double Helix" loading="lazy"></picture></div></div><div><div><picture><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_143cf1a441962c90f082d4f7dba2aeefb07f4e821.png?width=750&format=png&optimize=medium"><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_143cf1a441962c90f082d4f7dba2aeefb07f4e821.png?width=750&format=png&optimize=medium" media="(min-width: 600px)"><img src="https://main--aem-block-collection--adobe.hlx.live/media_143cf1a441962c90f082d4f7dba2aeefb07f4e821.png?width=750&format=png&optimize=medium" alt="Yellow Double Helix" loading="lazy"></picture></div><div><p>Or you can just view the preview</p><p><a href="/"><em>Preview</em></a></p></div></div></div></div><div><h2>Boilerplate Highlights?</h2><p>Find some of our favorite staff picks below:</p><div class="cards"><div><div><picture><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_16582eee85490fbfe6b27c6a92724a81646c2e649.jpeg?width=750&format=jpeg&optimize=medium"><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_16582eee85490fbfe6b27c6a92724a81646c2e649.jpeg?width=750&format=jpeg&optimize=medium" media="(min-width: 600px)"><img src="https://main--aem-block-collection--adobe.hlx.live/media_16582eee85490fbfe6b27c6a92724a81646c2e649.jpeg?width=750&format=jpeg&optimize=medium" alt="A fast-moving Tunnel" loading="lazy"></picture></div><div><p><strong>Unmatched speed</strong></p><p>Helix is the fastest way to publish, create, and serve websites</p></div></div><div><div><picture><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_17a5ca5faf60fa6486a1476fce82a3aa606000c81.jpeg?width=750&format=jpeg&optimize=medium"><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_17a5ca5faf60fa6486a1476fce82a3aa606000c81.jpeg?width=750&format=jpeg&optimize=medium" media="(min-width: 600px)"><img src="https://main--aem-block-collection--adobe.hlx.live/media_17a5ca5faf60fa6486a1476fce82a3aa606000c81.jpeg?width=750&format=jpeg&optimize=medium" alt="An iceberg" loading="lazy"></picture></div><div><p><strong>Content at scale</strong></p><p>Helix allows you to publish more content in shorter time with smaller teams</p></div></div><div><div><picture><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_162cf9431ac2dfd17fe7bf4420525bbffb9d0ccfe.jpeg?width=750&format=jpeg&optimize=medium"><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_162cf9431ac2dfd17fe7bf4420525bbffb9d0ccfe.jpeg?width=750&format=jpeg&optimize=medium" media="(min-width: 600px)"><img src="https://main--aem-block-collection--adobe.hlx.live/media_162cf9431ac2dfd17fe7bf4420525bbffb9d0ccfe.jpeg?width=750&format=jpeg&optimize=medium" alt="Doors with light in the dark" loading="lazy"></picture></div><div><p><strong>Uncertainty eliminated</strong></p><p>Preview content at 100% fidelity, get predictable content velocity, and shorten project durations</p></div></div><div><div><picture><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_136fdd3174ff44787179448cc2e0264af1b02ade9.jpeg?width=750&format=jpeg&optimize=medium"><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_136fdd3174ff44787179448cc2e0264af1b02ade9.jpeg?width=750&format=jpeg&optimize=medium" media="(min-width: 600px)"><img src="https://main--aem-block-collection--adobe.hlx.live/media_136fdd3174ff44787179448cc2e0264af1b02ade9.jpeg?width=750&format=jpeg&optimize=medium" alt="A group of people around a Table" loading="lazy"></picture></div><div><p><strong>Widen the talent pool</strong></p><p>Authors on Helix use Microsoft Word, Excel or Google Docs and need no training</p></div></div><div><div><picture><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_1cae8484004513f76c6bf5860375bc020d099a6d6.jpeg?width=750&format=jpeg&optimize=medium"><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_1cae8484004513f76c6bf5860375bc020d099a6d6.jpeg?width=750&format=jpeg&optimize=medium" media="(min-width: 600px)"><img src="https://main--aem-block-collection--adobe.hlx.live/media_1cae8484004513f76c6bf5860375bc020d099a6d6.jpeg?width=750&format=jpeg&optimize=medium" alt="HTML code in a code editor" loading="lazy"></picture></div><div><p><strong>The low-code way to developer productivity</strong></p><p>Say goodbye to complex APIs spanning multiple languages. Anyone with a little bit of HTML, CSS, and JS can build a site on Project Helix.</p></div></div><div><div><picture><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_11381226cb58caf1f0792ea27abebbc8569b00aeb.jpeg?width=750&format=jpeg&optimize=medium"><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_11381226cb58caf1f0792ea27abebbc8569b00aeb.jpeg?width=750&format=jpeg&optimize=medium" media="(min-width: 600px)"><img src="https://main--aem-block-collection--adobe.hlx.live/media_11381226cb58caf1f0792ea27abebbc8569b00aeb.jpeg?width=750&format=jpeg&optimize=medium" alt="A rocket and a headless suit" loading="lazy"></picture></div><div><p><strong>Headless is here</strong></p><p>Go directly from Microsoft Excel or Google Sheets to the web in mere seconds. Sanitize and collect form data at extreme scale with Project Helix Forms.</p></div></div><div><div><picture><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_18fadeb136e84a2efe384b782e8aea6e92de4fc13.jpeg?width=750&format=jpeg&optimize=medium"><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_18fadeb136e84a2efe384b782e8aea6e92de4fc13.jpeg?width=750&format=jpeg&optimize=medium" media="(min-width: 600px)"><img src="https://main--aem-block-collection--adobe.hlx.live/media_18fadeb136e84a2efe384b782e8aea6e92de4fc13.jpeg?width=750&format=jpeg&optimize=medium" alt="A dial with a hand on it" loading="lazy"></picture></div><div><p><strong>Peak performance</strong></p><p>Use Project Helix's serverless architecture to meet any traffic need. Use Project Helix's PageSpeed Insights Github action to evaluate every Pull-Request for Lighthouse Score.</p></div></div></div><p><br></p><div class="section-metadata"><div><div><p>Style</p></div><div><p>highlight</p></div></div></div></div><div><div class="metadata"><div><div><p>Title</p></div><div><p>Home | Helix Project Boilerplate</p></div></div><div><div><p>Image</p></div><div><picture><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_1dc0a2d290d791a050feb1e159746f52db392775a.jpeg?width=1200&format=pjpg&optimize=medium"><source srcset="https://main--aem-block-collection--adobe.hlx.live/media_1dc0a2d290d791a050feb1e159746f52db392775a.jpeg?width=1200&format=pjpg&optimize=medium" media="(min-width: 600px)"><img src="https://main--aem-block-collection--adobe.hlx.live/media_1dc0a2d290d791a050feb1e159746f52db392775a.jpeg?width=1200&format=pjpg&optimize=medium" loading="lazy"></picture></div></div><div><div><p>Description</p></div><div><p>Use this template repository as the starting point for new Helix projects.</p></div></div></div></div></main>
+  <footer></footer>
+</body>
+`;
+      const yDoc = new Y.Doc();
+      await aem2doc(html, yDoc);
+      const result = doc2aem(yDoc);
+      assert.equal(result, html);
+    });
+
+    it('Test more link roundtrip', async () => {
+      const html = `
+<body>
+  <header></header>
+  <main><div><p>Your forked repo is setup as a helix project and you are ready to start developing.<br>The content you are looking at is served from this <a href="https://drive.google.com/drive/folders/1Gwwrujv0Z4TxJM8askdqQkHSD969dGK7">gdrive</a><br><br>Adjust the <code>fstab.yaml</code> to point to a folder either in your sharepoint or your gdrive that you shared with helix. See the full tutorial here:<br><br><a href="https://bit.ly/3aImqUL">https://www.hlx.live/tutorial</a></p></div></main>
+  <footer></footer>
+</body>
+`;
+    const yDoc = new Y.Doc();
+    await aem2doc(html, yDoc);
+    const result = doc2aem(yDoc);
+    assert.equal(result, html);
+  });
+
+  it('Test nested marks roundtrip', async () => {
+    const html = `
+<body>
+  <header></header>
+  <main><div><p>Your forked repo is setup as a helix project and you are ready to start developing.<br>The content you are looking at is served <strong>from </strong><em><strong>this</strong></em> <a href="https://drive.google.com/drive/folders/1Gwwrujv0Z4TxJM8askdqQkHSD969dGK7">gdrive</a><br><br>Adjust the <code>fstab.yaml</code> to point to a folder either in your sharepoint or your gdrive that you shared with helix. See the full tutorial here:<br><br><a href="https://bit.ly/3aImqUL">https://www.hlx.live/tutorial</a></p></div></main>
+  <footer></footer>
+</body>
+`;
+  const yDoc = new Y.Doc();
+  await aem2doc(html, yDoc);
+  const result = doc2aem(yDoc);
+  assert.equal(result, html);
+});
+it('Test simple block roundtrip', async () => {
+  const html = `
+<body>
+  <header></header>
+  <main><div><div class="foo"><div><h1>bar</h1></div><div><h2>bar2</h2></div></div></div></main>
+  <footer></footer>
+</body>
+`;
+const yDoc = new Y.Doc();
+await aem2doc(html, yDoc);
+const result = doc2aem(yDoc);
+assert.equal(result, html);
+});
+});
+
