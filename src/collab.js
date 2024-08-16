3945cbe7df1c5a2812aa1cbc891e5dab2834ac39
Chris Peyer <chrischrischris@users.noreply.github.com>
2024-08-16 19:25:14 -0400

    Support for colspan attribute in tables (#46)

--- src/collab.js
+++ src/collab.js
@@ -247,7 +247,16 @@ export function aem2doc(html, ydoc) {
       }
 
       if (prop === 'getAttribute') {
-        return (name) => (target.properties ? target.properties[name] : undefined);
+        return (name) => {
+          if (name === 'colspan') {
+            // when `tree` is created using `fromHtml` in hast-util-from-html
+            // that then calls fromParse5 in hast-util-from-parse5
+            // which converts the `colspan` attribute to `colSpan`
+            // eslint-disable-next-line no-param-reassign
+            name = 'colSpan';
+          }
+          return target.properties ? target.properties[name] : undefined;
+        };
       }
 
       if (prop === 'hasAttribute') {
